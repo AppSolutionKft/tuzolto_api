19fab3e406637114e56ea0edee84d33170268a80
Mihaly Szabo <mihaly.szabo@appsolution.hu>
2024-06-06 13:35:07 +0200

    firebase upgrade

--- core/FCM.php
+++ core/FCM.php
@@ -15,15 +15,16 @@ class FCM
      * @param $title
      * @param $message
      * @param string $navigateTo
-     * @return false|string
+     * @return array
      * @throws FirebaseException
      * @throws MessagingException
      */
     public static function sendNotification($tokens, $fields)
     {
-        $result = array('success' => 0, 'failed' => 0);
+        $success = 0;
+        $result = array('success' => $success, 'failed' => 0);
         try {
-            $factory = (new Factory)->withServiceAccount($_SERVER["DOCUMENT_ROOT"] . '/modules/firebase/cred.json');
+            $factory = (new Factory)->withServiceAccount($_SERVER["DOCUMENT_ROOT"] . 'tuzolto-app-1591806201105-firebase-adminsdk-yr451-8895c47a95.json');
             $sToken = array();
             $uTokens = array_values(array_unique($tokens));
             if (count($uTokens) > 500) {
@@ -32,27 +33,34 @@ class FCM
                 $sToken[0] = $uTokens;
             }
             foreach ($sToken as $ktokens) {
+                file_put_contents('error.log', "ktokens: " . json_encode($ktokens) . "\r\n", FILE_APPEND);
+
                 $message = new RawMessageFromArray($fields);
                 $sendReport = ($factory->createMessaging())->sendMulticast($message, $ktokens);
                 foreach ($sendReport->successes()->getItems() as $sItem) {
+                    file_put_contents('error.log', json_encode($sItem->message()->jsonSerialize()) . "\r\n", FILE_APPEND);
                     try {
                         $token = $sItem->message()->jsonSerialize()["token"];
                     } catch (\Exception $e) {
                     }
                 }
                 foreach ($sendReport->failures()->getItems() as $sItem) {
+                    file_put_contents('error.log', json_encode($sItem->message()->jsonSerialize()) . "\r\n", FILE_APPEND);
                     try {
                         $token = $sItem->message()->jsonSerialize()["token"];
                     } catch (\Exception $e) {
                     }
                 }
+                $success = $sendReport->successes()->count();
                 $result = array(
                     'success' => $sendReport->successes()->count(),
                     'failed' => $sendReport->failures()->count(),
                 );
+                file_put_contents('error.log', json_encode($result) . "\r\n", FILE_APPEND);
             }
         } catch (\Exception $e) {
+            file_put_contents('error.log', $e->getMessage() . "\r\n", FILE_APPEND);
         }
-        return json_encode(array("status"=>200, "result"=>$result));
+        return array("status" => 200, "success" => $success, "result" => $result);
     }
 }
